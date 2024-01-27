82ef5125825e77a3858ec60bf71e4ae493ea63b9
Macky <104384493+ImMacky@users.noreply.github.com>
2024-01-27 10:41:23 -0700

    Update client.lua
    Didn't work as intended to, fixed now

--- client/client.lua
+++ client/client.lua
@@ -55,12 +55,12 @@ RegisterNetEvent('mk-DumpsterDepths:Client:HideInDumpster', function(source)
                                         SetEntityVisible(Player, true, 0)
                                         exports['qb-core']:HideText()
                                     end
-                            
+
                                     while Timer < Config.ElapsedTime and insideDumpster do
                                         Wait(1000)
                                         Timer = Timer + 1
                                     end
-                            
+
                                     if Timer >= Config.ElapsedTime then
                                         Timer = 0
                                         insideDumpster = false
